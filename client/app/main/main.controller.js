79a4766f340313e2d994ba941b44e58b16c37d47
orvisevans <b.orvis@gmail.com>
2015-09-25 17:13:43 -0700

    Moved tile properties to tile service

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('documentsApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
+  .controller('MainCtrl', function ($scope, $http, socket, tile) {
     $scope.awesomeThings = [];
 
     $http.get('/api/things').success(function(awesomeThings) {
@@ -9,34 +9,8 @@ angular.module('documentsApp')
       socket.syncUpdates('thing', $scope.awesomeThings);
     });
 
-    $scope.getColor = function($index) {
-      var _d = ($index + 1) % 11;
-      var bg = '';
-
-      switch(_d) {
-        case 1:       bg = 'red';         break;
-        case 2:       bg = 'green';       break;
-        case 3:       bg = 'darkBlue';    break;
-        case 4:       bg = 'blue';        break;
-        case 5:       bg = 'yellow';      break;
-        case 6:       bg = 'pink';        break;
-        case 7:       bg = 'darkBlue';    break;
-        case 8:       bg = 'purple';      break;
-        case 9:       bg = 'deepBlue';    break;
-        case 10:      bg = 'lightPurple'; break;
-        default:      bg = 'yellow';      break;
-      }
-
-      return bg;
-    };
-
-    $scope.getSpan = function($index) {
-      var _d = ($index + 1) % 11;
-
-      if (_d === 1 || _d === 5) {
-        return 2;
-      }
-    };
+    $scope.getColor = tile.getColor;
+    $scope.getSpan = tile.getSpan;
 
     $scope.deleteThing = function(thing) {
       $http.delete('/api/things/' + thing._id);
