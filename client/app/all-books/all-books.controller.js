7992ee11b59c03ec82e12dd9474175fffc968530
orvisevans <b.orvis@gmail.com>
2015-09-28 08:09:33 -0700

    Created all-books view

--- client/app/all-books/all-books.controller.js
+++ client/app/all-books/all-books.controller.js
@@ -1,6 +1,22 @@
 'use strict';
 
 angular.module('documentsApp')
-  .controller('AllBooksCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('AllBooksCtrl', function ($scope, $http, socket, tile) {
+    $scope.allBooks = [];
+
+    $http.get('/api/books').success(function(allBooks) {
+      $scope.allBooks = allBooks;
+      socket.syncUpdates('book', $scope.allBooks);
+    });
+
+    $scope.getColor = tile.getColor;
+    $scope.getSpan = tile.getSpan;
+
+    $scope.borrowBook = function(book) {
+      //TODO: Implement
+    };
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('book');
+    });
   });
