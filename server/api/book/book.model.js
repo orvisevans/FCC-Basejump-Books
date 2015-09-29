4347dc587595c3db1d0bc050dbb5fd59d6a87176
orvisevans <b.orvis@gmail.com>
2015-09-29 00:17:57 -0700

    set field requirements and defaults

--- server/api/book/book.model.js
+++ server/api/book/book.model.js
@@ -4,14 +4,14 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var BookSchema = new Schema({
-  name: String,
-  author: String,
-  coverUrl: String,
-  owner: String,
+  name: {type: String, required: true},
+  author: {type: String, required: true},
+  coverUrl: {type: String, required: true},
+  owner: {type: String, required: true},
   borrower: String,
-  dateAdded: Date,
-  public: Boolean,
-  active: Boolean
+  dateAdded: {type: Date, default: Date.now},
+  public: {type: Boolean, default: true},
+  active: {type: Boolean, default: true}
 });
 
 module.exports = mongoose.model('Book', BookSchema);
