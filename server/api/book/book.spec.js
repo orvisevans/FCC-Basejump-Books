5fa43181b7f381505ef2cc812aae20fee21467d5
Orvis <b.orvis@gmail.com>
2015-11-29 08:27:25 +0000

    started adding proper testing to book api

--- server/api/book/book.spec.js
+++ server/api/book/book.spec.js
@@ -1,3 +1,5 @@
+/*jshint mocha:true*/
+
 'use strict';
 
 var should = require('should');
@@ -5,6 +7,13 @@ var app = require('../../app');
 var request = require('supertest');
 
 describe('GET /api/books', function() {
+
+  before(function(done) {
+    require('../../config/seed');
+    setTimeout(function() {
+      done();
+    }, 1500);
+  });
 
   it('should respond with JSON array', function(done) {
     request(app)
@@ -14,6 +23,67 @@ describe('GET /api/books', function() {
       .end(function(err, res) {
         if (err) return done(err);
         res.body.should.be.instanceof(Array);
+        done();
+      });
+  });
+
+  it('should respond with 9 public books', function(done) {
+    request(app)
+      .get('/api/books')
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.length.should.be.exactly(8);
+        done();
+      })
+  });
+
+  it('should attach owner names to books', function(done) {
+    request(app)
+      .get('/api/books')
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.forEach(function(book) {
+          (book.owner.name === undefined).should.eql(false);
+        });
+        done();
+      });
+  });
+});
+
+describe('GET /api/books/:id', function() {
+  var testBook;
+
+  before(function(done) {
+    request(app)
+      .get('/api/books')
+      .end(function(err, res) {
+        if (err) return done(err);
+        testBook = res.body[0];
+        done();
+      });
+  });
+
+  it('should return JSON Object', function(done) {
+    request(app)
+      .get('/api/books/' + testBook._id)
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceof(Object);
+        done();
+      });
+  });
+
+  it('should return a book with at least author, coverUrl, dateAdded, and owner', function(done) {
+    request(app)
+      .get('/api/books/' + testBook._id)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.have.property('author');
+        res.body.should.have.property('coverUrl');
+        res.body.should.have.property('dateAdded');
+        res.body.should.have.property('owner');
         done();
       });
   });
